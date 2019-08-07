066f5eb8528b0346612cbea6f16ab58f9f52e525
heytitle <pat.chormai@gmail.com>
2019-08-07 14:25:00 +0700

    add tag a

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./header.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookF, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import A from "../components/a"
 
 const Header = () => {
   return <header class="site-header">
@@ -19,12 +20,12 @@ const Header = () => {
               margin: "5px 0",
             }}>
               <div class="social">
-                <a href="https://www.facebook.com/electinth">
+                <A href="https://www.facebook.com/electinth">
                   <FontAwesomeIcon icon={faFacebookF}/>
-                </a>
-                <a href="https://twitter.com/electinth">
+                </A>
+                <A href="https://twitter.com/electinth">
                   <FontAwesomeIcon icon={faTwitter}/>
-                </a>
+                </A>
               </div>
             </div>
             <div style={{clear: "both"}}></div>
