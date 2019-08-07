0ed2c5e3ccf85c29fb11420eb129f21b6f148d04
heytitle <pat.chormai@gmail.com>
2019-08-07 22:03:07 +0700

    style control box (#4)

--- src/components/control-box.js
+++ src/components/control-box.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 
 // @todo #2 implement control box (name search & filter)
 const ControlBox = ({namePlaceholder, onNameSearch}) => {
@@ -7,16 +10,33 @@ const ControlBox = ({namePlaceholder, onNameSearch}) => {
       onNameSearch(event.target.value)
     }
   }
-  return <div style={{border: "1px dotted"}}>
-    <span>
-        <input type="checkbox"/> ซ่อนประธานสภา
+  return <div style={{display: "table", fontWeight: "bold", width: "100%"}}>
+    <span style={{display: "table-cell", verticalAlign: "middle"}}>
+        <input type="checkbox"/> ประธานสภา
     </span>
-    <span style={{float: "right"}}>
-      <input type="text" 
-      defaultValue={namePlaceholder}
-      placeholder="ค้นหาจากชื่อ"
-      onKeyDown={onKeyDown}
-      /> <i>(กด Enter เพื่อค้นหา)</i>
+    <span style={{float: "right", display: "table-cell"}}>
+      <i style={{
+          fontWeight: "normal",
+          marginRight: "10px",
+        }}>
+          (กด Enter เพื่อค้นหา)
+      </i>
+      <span style={{
+          display: "inline-block",
+          padding: "5px 10px",
+          border: "2px solid",
+          borderRadius: "5px",
+        }}>
+        <input type="text" 
+        defaultValue={namePlaceholder}
+        placeholder="ค้นหาจากชื่อ"
+        onKeyDown={onKeyDown}
+        style={{border: "0px"}}
+        /> 
+        <span>
+          <FontAwesomeIcon icon={faSearch}/>
+        </span>
+      </span>
     </span>
   </div>
 }
