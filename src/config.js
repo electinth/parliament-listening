3f35463d56d17746d7511d567e487803fb0bbd85
Puripant <puripant@gmail.com>
2019-08-07 21:08:44 +0700

    Format dates

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   datasets: [
     {
-      name: "25/07/2562 แถลงนโยบาย​ฯ",
+      name: "25 ก.ค. 2562 แถลงนโยบาย​ฯ",
       file: "/data/25-26-07-2019.json" // @todo #2 update data
     }
   ]
