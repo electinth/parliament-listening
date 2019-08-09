623e8fc14a398a2113e7a8950032f973a7bd6801
heytitle <pat.chormai@gmail.com>
2019-08-09 12:51:18 +0700

    revise msg

--- src/message.js
+++ src/message.js
@@ -10,7 +10,7 @@ In VOTE We TRUST
 
 Thank you very much for being curious about how our project "Parliament Listening" works. :)
 
-We actually like that you inspect our code; however, you won't see much because it's minified. Because we know that there are people like you out there that would like to learn and help, that's why we publicly host our code on Github.
+We actually like that you inspect our code. We know that there are people like you out there that would like to learn and help. That's why we publicly host our code on Github.
 
 Github URL: github.com/codeforthailand/parliament-listening
 
