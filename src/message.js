a5e96afd27e76371052e06e247d86bc3b0ffb1fa
heytitle <pat.chormai@gmail.com>
2019-08-10 09:27:53 +0700

    update console-msg

--- src/message.js
+++ src/message.js
@@ -10,11 +10,11 @@ In VOTE We TRUST
 
 Thank you very much for being curious about how our project "Parliament Listening" works. :)
 
-We actually like that you inspect our code. We know that there are people, like you out there that would like to learn and help. That's why we publicly host our code on Github.
+We actually like that you inspect our code. We know that there are people like you out there that would like to learn and help. That's why we publicly host our code on Github.
 
-Github URL: github.com/codeforthailand/parliament-listening
+Here is our Github URL: github.com/codeforthailand/parliament-listening.
 
-Moreover, we're more than happy to recieve your comments and feedback. You can either send us through the Github repository or our social media accounts.
+Having said that, we're more than happy to recieve your comments and feedback. You can either send us through the Github repository or our social media accounts.
 
 Have a nice day and happy coding!
 `
