12e1ca7cc6caf14edfa0271f0899fe1fdeb57033
heytitle <pat.chormai@gmail.com>
2019-08-14 22:27:10 +0200

    fix window undefined

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,8 +1,20 @@
 import { useState, useEffect } from 'react'
 import { useQueryParam, StringParam, BooleanParam } from 'use-query-params'
 
+const windowLocation = (() => {
+  if(typeof window !== 'undefined' && window) {
+    return "" // useQueryParam will parse query params automatically
+  } else {
+    return "?dummy-param"
+  }
+})()
+
 const stateFromQueryParam = (name, type) => {
-    const [valueFromUrl, setQueryToUrl] = useQueryParam(name, type)
+    const [valueFromUrl, setQueryToUrl] = useQueryParam(
+        name,
+        type,
+        windowLocation
+    )
     const [stateValue, setStateValue] = useState(valueFromUrl)
 
     useEffect(() => {
