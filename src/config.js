70952c648a34403b3433d275ff641ce59765aefe
heytitle <pat.chormai@gmail.com>
2019-08-14 22:15:00 +0200

    introduce date in query params

--- src/config.js
+++ src/config.js
@@ -1,10 +1,19 @@
+const datasets = [
+  {
+    name: "25 ก.ค. 2562 แถลงนโยบาย​ฯ",
+    date: "25-07-2562"
+  }
+]
+
+const dateToDataset = {}
+
+datasets.forEach(d => {
+  dateToDataset[d.date] = d
+})
+
 const config = {
-  datasets: [
-    {
-      name: "25 ก.ค. 2562 แถลงนโยบาย​ฯ",
-      file: "/data/25-07-2019.json"
-    }
-  ]
+  datasets: datasets,
+  dateToDataset: (d) => dateToDataset[d]
 }
 
 export default config
